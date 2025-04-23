d71cb37afccdf51144d5497a35f89417052784c9
Ghaida <ghaidagh347@gmail.com>
2025-04-23 10:57:46 +0300

    creat profile page

--- Sportify/account/urls.py
+++ Sportify/account/urls.py
@@ -5,6 +5,7 @@ app_name="account"
 
 urlpatterns = [
     
-    path("signup/", views.sign_up_as_view,name="sign_up_as_view")
+    path("signup/", views.sign_up_as_view,name="sign_up_as_view"),
+    path("profile/", views.profile_view,name="profile_view"),
 ]
 
--- Sportify/account/views.py
+++ Sportify/account/views.py
@@ -3,5 +3,9 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
-def sign_up_as_view(request:HttpRequest):\
+def sign_up_as_view(request:HttpRequest):
     return render(request,"account/sign_up_as.html")
+
+def profile_view(request:HttpRequest):
+    return render(request,"account/profile.html")
+
