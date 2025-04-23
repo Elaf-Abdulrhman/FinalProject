3c2281039f714eb0c95dff4904e470f01da746e0
duha alshehri <alshehriduha7@gmail.com>
2025-04-23 10:42:19 +0300

    done with signup/signin

--- Sportify/Sportify/urls.py
+++ Sportify/Sportify/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('acount/' include('account.urls'))
+    path('account/', include('account.urls')),
     path('contact/', include('contact.urls')),
 ]
