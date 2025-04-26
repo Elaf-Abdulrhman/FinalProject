adfb0458fdc3e36f65bc750246e23fa3f05f2a50
elaf <elafabdualrhman00@gmail.com>
2025-04-26 15:32:28 +0300

    used moysar payment gateway for processing subscriptions, public url will be edited later in shaa'allah

--- Sportify/subscriptions/urls.py
+++ Sportify/subscriptions/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'subscriptions'
 
 urlpatterns = [
-    path('plus/', views.plus_plan, name='plus_plan'),
     path('payment/', views.payment, name='payment'),
+    path('payment_success/', views.payment_success, name='payment_success'),
+    path('plus_plan/', views.plus_plan, name='plus_plan'),
 ]
