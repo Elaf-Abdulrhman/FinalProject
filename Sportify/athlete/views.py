70b5e563eb834d85b9c892032094f8d425735de9
Ghaida <ghaidagh347@gmail.com>
2025-04-28 23:04:09 +0300

    athletes, club and search by username and name

--- Sportify/athlete/views.py
+++ Sportify/athlete/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from account.models import User, Athlete
+from django.db.models import Q
 # Create your views here.
 def all_athletes(request):
-    # This is a placeholder view function. You can replace it with your actual logic.
-    return render(request, 'athlete/all_athletes.html')
+    athletes=Athlete.objects.all()
+    search_query = request.GET.get("q", "")
+    if search_query:
+        athletes = athletes.filter(
+            Q(user__first_name__icontains=search_query) |
+            Q(user__username__icontains=search_query)
+        )
+
+    return render(request, 'athlete/all_athletes.html',{"athletes":athletes})
+
+
 
