77b4745c94b127fa513e27f3d9367ccf650b4b89
duha alshehri <alshehriduha7@gmail.com>
2025-04-29 13:26:08 +0300

    Admin dashboard is restricted to only the admin now

--- Sportify/admins/views.py
+++ Sportify/admins/views.py
@@ -3,4 +3,9 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboard(request):
-    return render(request, 'admins/dashboard.html')
+    if request.user.is_superuser:
+
+        return render(request, 'admins/dashboard.html')
+    else:
+
+        return render(request, 'admins/notadmin.html')
