83f2b3e4033011b62e0e6fd0acf1df4eeaefb31d
Ghaida <ghaidagh347@gmail.com>
2025-05-01 13:22:15 +0300

    Admin Redirect

--- Sportify/admins/views.py
+++ Sportify/admins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 @login_required
@@ -9,3 +9,6 @@ def dashboard(request):
     else:
 
         return render(request, 'admins/notadmin.html')
+
+# def to_dashboard(request):
+#     return redirect('admins:dashboard')
