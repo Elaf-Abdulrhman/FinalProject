2952c29f3e2c0ebadabb9feff2d26ae8a1dbe388
duha alshehri <alshehriduha7@gmail.com>
2025-05-03 14:27:56 +0300

    added filtring in all athlete and all clubs  pages and fixed design

--- Sportify/Sportify/urls.py
+++ Sportify/Sportify/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import settings
+from django.views.i18n import set_language
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,4 +34,8 @@ urlpatterns = [
     path('subscriptions/', include('subscriptions.urls')),
     path('bookmarks/', include('bookmarks.urls')),
     path('admins/', include('admins.urls')),
+
+
+
+    path('i18n/setlang/', set_language, name='set_language'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
